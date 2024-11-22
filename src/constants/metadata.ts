1f011731fd1d2fe403ad651a05043307738902d7
Jin Young Choi <jinyoungchoi@alumni.emory.edu>
2024-11-22 01:30:18 -0500

    scrubbed specific podcast data

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -1,15 +1,14 @@
 /* AWS constants */
 const awsRegion = "us-east-1"; // choose the one closest to you, more info: https://aws.amazon.com/about-aws/global-infrastructure/regions_az/
-const podcastUrl = "http://sumofallpeople.com";
-const s3BucketName = "sumofallpeople.com";
+const podcastUrl = "<REPLACE_ME_WITH_PODCAST_URL>";
+const s3BucketName = "<REPLACE_ME_WITH_S3_BUCKET_NAME>";
 
 /* show constants */
-const showTitle = "Sum Of All People";
-const showDescription =
-  "'You are the sum of five people you surround yourself with': an all too common saying that underscores the impact a select group of people can have on one's life.\n\n In Sum Of All People, Jin Young invites a wide array of individuals who have left an imprint on his life and aims to expand on the aforementioned phrase; for it is the sum total of all people that, in ways big or small, hold the potential to shape the trajectory of each of our lives.";
-const showHostName = "Jin Young Choi";
-const showHostEmail = "jinyoungchoi@alumni.emory.edu";
-const showCoverArtURL = "http://sumofallpeople.com/media/SOAP.png"; // this can also point to the file in S3.
+const showTitle = "<REPLACE_ME_WITH_SHOW_TITLE>";
+const showDescription ="<REPLACE_ME_WITH_SHOW_DESCRIPTION";
+  const showHostName = "<REPLACE_ME_WITH_HOST_NAME>";
+  const showHostEmail = "<REPLACE_ME_WITH_EMAIL_ADDRESS>";
+  const showCoverArtURL = "<REPLACE_ME_WITH_IMAGE_URL>"; // this can also point to the file in S3.
 
 /* Apple Podcasts constants  */
 const iTunesCategory = "Society &amp; Culture"; // refer to https://podcasters.apple.com/support/1691-apple-podcasts-categories
